ba1cc96c02aa88a6992ce3f83ea635c081909332
Maximilian Persson <maxipersson05@gmail.com>
2022-09-01 14:31:22 +0200

    kuyfkuyfyf

---
+++ TE21B_Lucktext/Program.cs
@@ -0,0 +1,28 @@
+﻿string name;
+
+
+Console.WriteLine("Vad heter du?");
+name = Console.ReadLine();
+
+Console.WriteLine($"{name} Vilket ovanligt namn");
+Console.ReadLine();
+
+string sport;
+
+Console.WriteLine("Vilken är din favoritsport?");
+sport = Console.ReadLine();
+
+Console.WriteLine($"Okej du tycker om {sport} Vilken är din favorit liga?");
+Console.ReadLine();
+
+
+
+string spel;
+
+Console.WriteLine("Vilken är din favoritspel?");
+spel = Console.ReadLine();
+
+
+Console.WriteLine($"Tja {name} du tycker alltså om {spel}, har du spelat andra spel?");
+Console.ReadLine();
+
